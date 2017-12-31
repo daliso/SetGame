998b41baf9fb6e0f0f6ae095e39f27d7fa5fa914
daliso <daliso.zuze@gmail.com>
2017-12-31 11:52:52 -0500

    Added controller for main game play

---
+++ SetGame/Controllers/SetGameViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SetGameViewController.swift
+//  SetGame
+//
+//  Created by Daliso Zuze on 12/31/17.
+//  Copyright © 2017 Daliso Zuze. All rights reserved.
+//
+
+import UIKit
+
+class SetGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "Play Game!"
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
